967769a43a86a54cffffee3e36e2ffe7982b4fc9
vmanke <77922288+vmanke@users.noreply.github.com>
2024-01-23 10:56:25 +0100

    updated coloor theme

--- init.lua
+++ init.lua
@@ -40,6 +40,7 @@ vim.g.mapleader = " "
 
 require("lazy").setup({
 	"mfussenegger/nvim-dap",
+	"polirritmico/monokai-nightasty.nvim",
 	"nvim-lua/plenary.nvim",
 	"tomasr/molokai",
 	"cocopon/iceberg.vim",
@@ -185,10 +186,57 @@ local on_attach = function(client)
     require('completion').on_attach(client)
 end
 
+vim.opt.background = "dark" -- default to dark or light style
+
+require("monokai-nightasty").setup({
+    dark_style_background = "transparent", -- default, dark, transparent, #color
+    light_style_background = "default", -- default, dark, transparent, #color
+    terminal_colors = true, -- Set the colors used when opening a `:terminal`
+    color_headers = true, -- Enable header colors for each header level (h1, h2, etc.)
+    hl_styles = {
+        -- Style to be applied to different syntax groups. See `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = { italic = false },
+        variables = { italic = false },
+        -- Background styles for sidebars (panels) and floating windows:
+        floats = "default", -- default, dark, transparent
+        sidebars = "default", -- default, dark, transparent
+    },
+    sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+
+    hide_inactive_statusline = false, -- Hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+    dim_inactive = false, -- dims inactive windows
+    lualine_bold = true, -- Lualine headers will be bold or regular.
+    lualine_style = "dark", -- "dark", "light" or "default" (Follows dark/light style)
+
+    --- You can override specific color/highlights. Current values in `extras/palettes`
+
+    ---@param colors ColorScheme
+    on_colors = function(colors)
+        colors.border = colors.grey
+        colors.comment = "#2d7e79"
+    end,
+
+    ---@param highlights Highlights
+    ---@param colors ColorScheme
+    on_highlights = function(highlights, colors)
+        highlights.TelescopeNormal = { fg = colors.magenta, bg = colors.charcoal }
+        highlights.WinSeparator = { fg = colors.grey }
+    end,
+})
+
+-- Toggle Dark/Light styles
+vim.keymap.set(
+    {"n", "v"}, "<leader>tl", "<CMD>MonokaiToggleLight<CR>",
+    {silent = true, desc = "Monokai-NighTasty: Toggle light/dark theme"}
+)
+
+vim.cmd([[colorscheme monokai-nightasty]])
 vim.cmd([[
 " COLORSCHEMES:
 
-colorscheme molokai
+""" colorscheme molokai
 " set background=dark
 " colorscheme iceberg
 " colorscheme blackops
