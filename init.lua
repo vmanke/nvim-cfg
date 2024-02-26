2830b90c89c124983ed37829ff60be7d8d1c9b57
vmanke <77922288+vmanke@users.noreply.github.com>
2024-02-26 17:53:42 +0100

    telescope: open multi-selection

--- init.lua
+++ init.lua
@@ -176,14 +176,14 @@ else
             ft = { 'rust' },
         },
         {
-		      'akinsho/flutter-tools.nvim',
-		      lazy = false,
-		      dependencies = {
-		          'nvim-lua/plenary.nvim',
-		          'stevearc/dressing.nvim', -- optional for vim.ui.select
-		      },
-		      config = true,
-		},
+              'akinsho/flutter-tools.nvim',
+              lazy = false,
+              dependencies = {
+                  'nvim-lua/plenary.nvim',
+                  'stevearc/dressing.nvim', -- optional for vim.ui.select
+              },
+              config = true,
+        },
         'dcampos/nvim-snippy',
         {
             'hrsh7th/nvim-cmp',
@@ -296,93 +296,93 @@ else
     -- flutter:
     
     
-	-- alternatively you can override the default configs
-	require("flutter-tools").setup({
+    -- alternatively you can override the default configs
+    require("flutter-tools").setup({
         ui = {
-	      	-- the border type to use for all floating windows, the same options/formats
-	      	-- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
-	      	border = "rounded",
-	      	-- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
-	      	-- please note that this option is eventually going to be deprecated and users will need to
-	      	-- depend on plugins like `nvim-notify` instead.
-	      	notification_style = 'native'
+            -- the border type to use for all floating windows, the same options/formats
+            -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+            border = "rounded",
+            -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+            -- please note that this option is eventually going to be deprecated and users will need to
+            -- depend on plugins like `nvim-notify` instead.
+            notification_style = 'native'
        },
        decorations = {
-	      	statusline = {
-	      	    -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
-	      	    -- this will show the current version of the flutter app from the pubspec.yaml file
-	      	    app_version = false,
-	      	    -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
-	      	    -- this will show the currently running device if an application was started with a specific
-	      	    -- device
-	      	    device = false,
-	      	    -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
-	      	    -- this will show the currently selected project configuration
-	      	    project_config = false,
-	      	}
+            statusline = {
+                -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+                -- this will show the current version of the flutter app from the pubspec.yaml file
+                app_version = false,
+                -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+                -- this will show the currently running device if an application was started with a specific
+                -- device
+                device = false,
+                -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+                -- this will show the currently selected project configuration
+                project_config = false,
+            }
        },
        debugger = { -- integrate with nvim dap + install dart code debugger
-	      	enabled = false,
-	      	run_via_dap = false, -- use dap instead of a plenary job to run flutter apps
-	      	-- if empty dap will not stop on any exceptions, otherwise it will stop on those specified
-	      	-- see |:help dap.set_exception_breakpoints()| for more info
-	      	exception_breakpoints = {},
+            enabled = false,
+            run_via_dap = false, -- use dap instead of a plenary job to run flutter apps
+            -- if empty dap will not stop on any exceptions, otherwise it will stop on those specified
+            -- see |:help dap.set_exception_breakpoints()| for more info
+            exception_breakpoints = {},
        },
        flutter_path = "C:\\FlutterSDK\\flutter\\bin\\flutter", -- <-- this takes priority over the lookup
        -- flutter_lookup_cmd = "which flutter", -- example "dirname $(which flutter)" or "asdf where flutter"
        root_patterns = { ".git", "pubspec.yaml" }, -- patterns to find the root of your flutter project
        fvm = false, -- takes priority over path, uses <workspace>/.fvm/flutter_sdk if enabled
        widget_guides = {
-	      	enabled = false,
+            enabled = false,
        },
        closing_tags = {
-	      	highlight = "ErrorMsg", -- highlight for the closing tag
-	      	prefix = ">", -- character to use for close tag e.g. > Widget
-	      	enabled = true -- set to false to disable
+            highlight = "ErrorMsg", -- highlight for the closing tag
+            prefix = ">", -- character to use for close tag e.g. > Widget
+            enabled = true -- set to false to disable
        },
        dev_log = {
-	      	enabled = true,
-	      	notify_errors = false, -- if there is an error whilst running then notify the user
-	      	open_cmd = "tabedit", -- command to use to open the log buffer
+            enabled = true,
+            notify_errors = false, -- if there is an error whilst running then notify the user
+            open_cmd = "tabedit", -- command to use to open the log buffer
        },
        dev_tools = {
-	      	autostart = false, -- autostart devtools server if not detected
-	      	auto_open_browser = false, -- Automatically opens devtools in the browser
+            autostart = false, -- autostart devtools server if not detected
+            auto_open_browser = false, -- Automatically opens devtools in the browser
        },
        outline = {
-	      	open_cmd = "30vnew", -- command to use to open the outline buffer
-	      	auto_open = false -- if true this will open the outline automatically when it is first populated
+            open_cmd = "30vnew", -- command to use to open the outline buffer
+            auto_open = false -- if true this will open the outline automatically when it is first populated
        },
        lsp = {
-	      	color = { -- show the derived colours for dart variables
-	      	    enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-	      	    background = false, -- highlight the background
-	      	    background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-	      	    foreground = false, -- highlight the foreground
-	      	    virtual_text = true, -- show the highlight using virtual text
-	      	    virtual_text_str = "■", -- the virtual text character to highlight
-	      	},
-	      	-- see the link below for details on each option:
-	      	-- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
-	      	settings = {
-	      	    showTodos = true,
-	      	    completeFunctionCalls = true,
-	      	    renameFilesWithClasses = "prompt", -- "always"
-	      	    enableSnippets = true,
-	      	    updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
-	      	}
+            color = { -- show the derived colours for dart variables
+                enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+                background = false, -- highlight the background
+                background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+                foreground = false, -- highlight the foreground
+                virtual_text = true, -- show the highlight using virtual text
+                virtual_text_str = "■", -- the virtual text character to highlight
+            },
+            -- see the link below for details on each option:
+            -- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
+            settings = {
+                showTodos = true,
+                completeFunctionCalls = true,
+                renameFilesWithClasses = "prompt", -- "always"
+                enableSnippets = true,
+                updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+            }
        }
-	})
+    })
 
     vim.cmd([[
         " Show hover
-		nnoremap K <Cmd>lua vim.lsp.buf.hover()<CR>
-		 " Jump to definition
-		nnoremap gd <Cmd>lua vim.lsp.buf.definition()<CR>
-		 " Open code actions using the default lsp UI, if you want to change this please see the plugins above
-		nnoremap <leader>ca <Cmd>lua vim.lsp.buf.code_action()<CR>
-		 " Open code actions for the selected visual range
-		xnoremap <leader>ca <Cmd>lua vim.lsp.buf.range_code_action()<CR>
+        nnoremap K <Cmd>lua vim.lsp.buf.hover()<CR>
+         " Jump to definition
+        nnoremap gd <Cmd>lua vim.lsp.buf.definition()<CR>
+         " Open code actions using the default lsp UI, if you want to change this please see the plugins above
+        nnoremap <leader>ca <Cmd>lua vim.lsp.buf.code_action()<CR>
+         " Open code actions for the selected visual range
+        xnoremap <leader>ca <Cmd>lua vim.lsp.buf.range_code_action()<CR>
     ]])
 
     require("glow").setup({
@@ -510,6 +510,33 @@ else
             end,
     }
 
+    -- ----------------------------------------------------------------------
+
+    local select_one_or_multi = function(prompt_bufnr)
+        local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+            require('telescope.actions').close(prompt_bufnr)
+            for _, j in pairs(multi) do
+                if j.path ~= nil then
+                    vim.cmd(string.format('%s %s', 'edit', j.path))
+                end
+            end
+        else
+            require('telescope.actions').select_default(prompt_bufnr)
+        end
+    end
+
+    require('telescope').setup {
+        defaults = {
+            mappings = {
+                i = {
+                    ['<CR>'] = select_one_or_multi,
+                }
+            }
+        }
+    }
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -519,6 +546,8 @@ else
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = "Find Word under Cursor"})
     vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = "Search Git Commits"})
     vim.keymap.set('n', '<leader>gb', builtin.git_bcommits, { desc = "Search Git Commits for Buffer"})
+
+    -- -----------------------------------------------------------------------------------------------------
 
     require("marks").setup({
         default_mappings = true,
